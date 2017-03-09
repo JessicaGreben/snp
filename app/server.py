df4ca2f695d18e8df86514ab41bdee484d657b54
Jessica Grebenschikov <Jessica@bright.md>
2017-03-08 17:02:58 -0800

    not sure where this code went. its up on the site, but wasnt in the code base so adding it now

--- app/server.py
+++ app/server.py
@@ -2,38 +2,39 @@ import os
 import jinja2
 from klein import Klein
 
+
 app = Klein()
 app.templates = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
+
 @app.route('/')
 def home(request):
-    """ get home page """
+    """ render home page """
     page = app.templates.get_template('index.html')
     return page.render()
 
-@app.route('/invest')
+@app.route('/invest/')
 def invest(request, init):
     """ how much is the initial investment worth after investing """
     from invest import Investment
+
     investment = Investment()
     accum = investment.calculateCompoundInterest(init)
     page = app.templates.get_template('invest.html')
     return page.render(initInvest=init, accumulatedInvest=accum)
 
-@app.route('/initInvest/submit', methods=['POST'])
+@app.route('/initInvest/submit/', methods=['POST'])
 def initInvestSubmit(request):
     """ get initial investment value from form """
     initInvest = int(request.args.get('initInvest')[0])
     return invest(request, initInvest)
 
+@app.route('/resources/')
+def learnToInvest(request):
+    """ render resource page """
+    page = app.templates.get_template('resources.html')
+    return page.render()
+
+
 if __name__ == "__main__":
-    # `Klein.run(host, port)` is doing this:
-    # 1. Runs a minimal twisted.web server 
-    # 2. Runs the default twisted reactor
-    # where port is the TCP port to accept HTTP requests
-    # and the host is used to bind the listening socket
-    # endpoint_description = "tcp:port=port:interface=host"
-    # endpoint = endpoints.serverFromString(reactor, endpoint_description)
-    # endpoint.listen(Site(self.resource()))
-    # reactor.run()
-    app.run('0.0.0.0', 8080)
+    app.run('127.0.0.1', 8080)
